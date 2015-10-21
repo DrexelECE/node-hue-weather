ddb59d004655c95191c8ba7eaf8c3d5a8067a3ef
Greg Yeutter <yeutterg@gmail.com>
2015-10-21 13:17:15 -0400

    template for color script

---
+++ color.js
@@ -0,0 +1,20 @@
+/**
+ * Returns RGB values for the temperature and precipitation
+ *
+ * @param error If any error
+ * @param temp The temperature in Fahrenheit
+ * @param precip The chance of precipitation in %
+ *
+ * @callback The RGB values for the temperature and precipitation
+ */
+module.exports = function(temp, precip, callback) {
+  var error = false;
+  if (error) callback(error, null, null);
+
+  // TODO James implement the logic for converting values to colors
+
+  var tempRGB = [255,0,0];
+  var precRGB = [0,255,0];
+
+  callback(null, tempRGB, precRGB);
+}
