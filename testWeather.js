a52895682e07ed442e764dcf6fb7857dbca5c689
Greg Yeutter <yeutterg@gmail.com>
2015-10-21 13:16:39 -0400

    random temp/precip generation for testing

---
+++ testWeather.js
@@ -0,0 +1,18 @@
+/**
+ * Generates random temperature and precipitation values for testing purposes
+ *
+ * @param location (ignored)
+ *
+ * @callback The temperature in Fahrenheit and the precipitation percentage
+ */
+module.exports = function (location, callback) {
+
+  // random integer values between 0 and 100
+  var tempFahrenheit = Math.round(Math.random() * 100);
+  var precipPercent = Math.round(Math.random() * 100);
+
+  console.log("Temperature: " + tempFahrenheit +
+    " F, Precipitation: " + precipPercent + "%." );
+
+  callback(null, tempFahrenheit, precipPercent);
+}
