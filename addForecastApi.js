6d52ec2e6d544b6f77654fefec76404c28faefac
Greg Yeutter <yeutterg@gmail.com>
2015-10-24 18:03:15 -0400

    working

---
+++ addForecastApi.js
@@ -0,0 +1,11 @@
+var props = require('./addToProperties')
+
+addApiKey();
+
+/**
+ * Adds Forecast.io API key to properties.json file
+ */
+function addApiKey() {
+    if (process.argv[2]) props.addProp('forecast_io_api_key', process.argv[2]);
+    else console.log("Error: Invalid input.");
+};
