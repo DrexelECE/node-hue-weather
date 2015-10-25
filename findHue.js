fdf1f04e710906f748735edde320b4ea9d3207f0
Greg Yeutter <yeutterg@gmail.com>
2015-10-25 15:06:23 -0400

    register program with bridge

--- findHue.js
+++ findHue.js
@@ -1,12 +1,21 @@
 var hue = require("node-hue-api");
 var props = require("./addToProperties")
 
+var HueApi = hue.HueApi;
+var username = 'nodeHueWeather';
+var api;
+
 function addBridge (bridge) {
     var ip = bridge[0]["ipaddress"];
-
-    props.addProp('philips_hue_bridge_ip', ip)
-
+    api = new HueApi(ip, username);
+    api.config().then(displayResult).done();
+    props.addProp('philips_hue_bridge_ip', ip);
     console.log("Bridge with IP: " + ip + " added.");
 };
+
+var displayResult = function(result) {
+    console.log(JSON.stringify(result, null, 2));
+};
+
 
 hue.nupnpSearch().then(addBridge).done();
