6d4bf64661238ec928871a5589ffb49864079e89
Greg Yeutter <yeutterg@gmail.com>
2015-11-01 18:34:19 -0500

    convert to 16 bit

--- color.js
+++ color.js
@@ -1,5 +1,5 @@
 /**
- * Returns RGB values for the temperature and precipitation
+ * Returns 16-bit hue values for the temperature and precipitation
  *
  * @param temp The temperature in Fahrenheit
  * @param precip The chance of precipitation in % (meaning range of, 0.0 - 1.0)
@@ -12,9 +12,9 @@ module.exports = function(temp, precip, callback) {
   if (isNaN(precip)) callback(new Error("precip is not numeric"), null, null);
   if (isNaN(temp)) callback(new Error("temp is not numeric"), null, null);
 
-  var hueTemp = Math.floor(170 + (0.85 * temp)); // blue (0 degF) to red (100 degF), via magenta
+  var hueTemp = Math.floor(43520 + (0.85 * temp * 255)); // blue (0 degF) to red (100 degF), via magenta
 
-  var huePrec = Math.floor(42 + (85 * precip)); // yellow (0%) to cyan (100%), via green
+  var huePrec = Math.floor(10710 + (85 * precip * 255)); // yellow (0%) to cyan (100%), via green
 
   callback(null, hueTemp, huePrec);
 };
